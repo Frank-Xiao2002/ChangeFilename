5a6ada8c47c8f5299bd6c7186f2b89c539c25396
Frank Xiao <Xiao670127@126.com>
2024-03-28 00:22:15 +0800

    finish main function

--- src/main/java/com/xxj/Main.java
+++ src/main/java/com/xxj/Main.java
@@ -2,6 +2,15 @@ package com.xxj;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PropertyReader propertyReader = new PropertyReader();
+        String folderPath = propertyReader.getProperty("folderpath");
+        String stringToBeDeleted = propertyReader.getProperty("stringToBeDeleted.last");
+        boolean result = FilenameChanger.changePDFFilename(folderPath, stringToBeDeleted);
+        if (result) {
+            System.out.println("All files have been renamed successfully!");
+        } else {
+            System.err.println("Error occurred while renaming files. Please check the folder path and the string to " +
+                    "be deleted.");
+        }
     }
 }
